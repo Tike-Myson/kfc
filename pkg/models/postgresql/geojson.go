bc538f1d0a7d0d551e0edb795ec4099a818337ce
Tike-Myson <asanxan.nurtilek@bk.ru>
2021-06-01 12:13:57 +0600

    last level

--- pkg/models/postgresql/geojson.go
+++ pkg/models/postgresql/geojson.go
@@ -3,7 +3,6 @@ package postgresql
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	geojson "github.com/paulmach/go.geojson"
 )
 
@@ -50,28 +49,27 @@ func (m *GeojsonModel) Get() (*geojson.FeatureCollection, error) {
 
 func (m *GeojsonModel) Search(fc *geojson.FeatureCollection) (*geojson.FeatureCollection, error){
 	fcResult := geojson.NewFeatureCollection()
-	fmt.Println(fc)
 	for _, v := range fc.Features {
 		e, err := json.Marshal(v.Geometry)
 		if err != nil {
-			fmt.Println(string(e))
-			rows, err := m.DB.Query(`SELECT "id", ST_AsGeoJSON("geom") FROM geometries WHERE ST_Intersects(geom, $1)`, string(e))
+			return nil, err
+		}
+		rows, err := m.DB.Query(`SELECT "id", ST_AsGeoJSON("geom") FROM geometries WHERE ST_Intersects(geom, $1)`, string(e))
+		if err != nil {
+			return nil, err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var id int
+			var geom *geojson.Geometry
+			err = rows.Scan(&id, &geom)
 			if err != nil {
 				return nil, err
 			}
-			defer rows.Close()
-
-			for rows.Next() {
-				var id int
-				var geom *geojson.Geometry
-				err = rows.Scan(&id, &geom)
-				if err != nil {
-					return nil, err
-				}
-				f := geojson.NewFeature(geom)
-				f.ID = id
-				fcResult = fcResult.AddFeature(f)
-			}
+			f := geojson.NewFeature(geom)
+			f.ID = id
+			fcResult = fcResult.AddFeature(f)
 		}
 	}
 	return fcResult, nil
